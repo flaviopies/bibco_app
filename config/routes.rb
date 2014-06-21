55330561a9eaa53fed116c75f84a6dea831d3af2
flaviopies <flaviopies@poli.ufrj.br>
2014-06-21 16:57:47 -0300

    Add user micropost

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
     
   root  'static_pages#home'
   match '/static_pages/home', to: 'static_pages#home', via: 'get'
